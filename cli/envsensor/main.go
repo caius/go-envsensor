2a7b30223b1512d1a9f099418400f708a1f49f6a
Caius Durling <dev@caius.name>
2018-02-09 02:12:27 +0000

    CLI tool sets up sensor & web server

--- cli/envsensor/main.go
+++ cli/envsensor/main.go
@@ -9,19 +9,21 @@ import (
 func main() {
 	fmt.Println("Welcome to temphumid, where it is our pleasure to probe you today.")
 
-	readings := make(chan envsensor.Reading)
+	// Configuration Section
+	// TODO: turn these into cli flags
+	// TODO: sensorVersion := 11 // 11 or 22 (DHT11, DHT22)
+	gpioPin := 17
+	probeDelay := 10              // seconds
+	cacheDelay := time.Minute * 1 // return cached data for 1 minute
 
-	// Grab the readings every second
+	// Program internals now
+	readingsChan := make(chan envsensor.Reading)
+
+	// Grab the readings every ten seconds
 	sensor := envsensor.DHTSensor{}
-	sensor.Start(17, readings, 1)
+	go sensor.Start(gpioPin, readingsChan, probeDelay)
 
-	go func() {
-		for r := range readings {
-			fmt.Printf("Got reading! t=%f, h=%f\n", r.Temperature, r.Humidity)
-		}
-	}()
-
-	// Fake waiting for stuff
-	time.Sleep(time.Second * time.Duration(2))
-	sensor.Stop()
+	// Serve readings, caching data up to a minute
+	server := envsensor.WebServer{}
+	server.Start(readingsChan, cacheDelay)
 }
