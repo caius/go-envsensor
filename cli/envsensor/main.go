6cb9bc87ee38f02c0becee6a384ecdecb4c7cdad
Caius Durling <dev@caius.name>
2018-02-11 10:55:14 +0000

    CLI flags & logging for envsensor

--- cli/envsensor/main.go
+++ cli/envsensor/main.go
@@ -1,29 +1,69 @@
 package main
 
 import (
+	"flag"
 	"fmt"
+	"github.com/bhoriuchi/go-bunyan/bunyan"
 	"github.com/caius/go-envsensor/internal/envsensor"
+	"os"
 	"time"
 )
 
-func main() {
-	fmt.Println("Welcome to temphumid, where it is our pleasure to probe you today.")
+type Configuration struct {
+	ProbeDelay    time.Duration
+	CacheDuration time.Duration
+	SensorPin     int
+	SensorVersion int
+	WebPort       int
+	Logger        bunyan.Logger
+	Verbose       bool
+}
 
+func createLogger(verbose bool) bunyan.Logger {
+	logLevel := bunyan.LogLevelInfo
+	if verbose {
+		logLevel = bunyan.LogLevelDebug
+	}
+	loggerConfig := bunyan.Config{
+		Name:   "envsensor",
+		Stream: os.Stdout,
+		Level:  logLevel,
+	}
+
+	log, err := bunyan.CreateLogger(loggerConfig)
+	if err != nil {
+		panic(err)
+	}
+
+	return log
+}
+
+func main() {
 	// Configuration Section
-	// TODO: turn these into cli flags
-	// TODO: sensorVersion := 11 // 11 or 22 (DHT11, DHT22)
-	gpioPin := 17
-	probeDelay := 10              // seconds
-	cacheDelay := time.Minute * 1 // return cached data for 1 minute
+	config := new(Configuration)
+
+	flag.DurationVar(&config.ProbeDelay, "poll", (time.Second * 10), "How often to poll the sensor for a reading. Default 10s")
+	flag.DurationVar(&config.CacheDuration, "cache", (time.Second * 60), "Max seconds to cache data for. Default 60s")
+	flag.IntVar(&config.SensorPin, "sensor-pin", 17, "GPIO Pin (Physical number) to communicate to sensor via")
+	flag.IntVar(&config.SensorVersion, "sensor-version", 11, "Which DHT sensor to talk to. 11 or 22.")
+	flag.IntVar(&config.WebPort, "web-port", 8080, "Port for webserver to listen on")
+	flag.BoolVar(&config.Verbose, "verbose", false, "Verbose output")
+
+	flag.Parse()
+
+	config.Logger = createLogger(config.Verbose)
+
+	config.Logger.Info("Welcome to envsensor, where it is our pleasure to probe you today.")
 
 	// Program internals now
 	readingsChan := make(chan envsensor.Reading)
 
-	// Grab the readings every ten seconds
-	sensor := envsensor.DHTSensor{}
-	go sensor.Start(gpioPin, readingsChan, probeDelay)
+	// Start reading from sensor
+	sensor := envsensor.NewDHTSensor(config.SensorVersion, config.SensorPin, config.ProbeDelay)
+	go sensor.Start(readingsChan)
 
 	// Serve readings, caching data up to a minute
 	server := envsensor.WebServer{}
-	server.Start(readingsChan, cacheDelay)
+	port := fmt.Sprintf(":%d", int(config.WebPort))
+	server.Start(readingsChan, port, config.CacheDuration)
 }
