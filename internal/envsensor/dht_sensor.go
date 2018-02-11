5ac636cf126897c57ed08d52d94b597afd09e1a2
Caius Durling <dev@caius.name>
2018-02-11 10:55:03 +0000

    DHTSensor is configurable

--- internal/envsensor/dht_sensor.go
+++ internal/envsensor/dht_sensor.go
@@ -1,28 +1,39 @@
 package envsensor
 
 import (
+	"fmt"
 	"time"
 )
 
 type DHTSensor struct {
 	Emitting     bool
-	pin          int
+	Pin          int
+	Delay        time.Duration
+	Version      int
 	readingsChan chan<- Reading
 	ticker       *time.Ticker
 }
 
+func NewDHTSensor(version int, pin int, delay time.Duration) DHTSensor {
+	return DHTSensor{
+		Version: version,
+		Pin:     pin,
+		Delay:   delay,
+	}
+}
+
 // Start emitting sensor readings into `readingsChan` channel.
 //
-// Only reads at most every `delay` seconds from the sensor
-func (s *DHTSensor) Start(pin int, readingsChan chan<- Reading, delay int) {
+// Only reads at most every `Delay` seconds from the sensor
+func (s *DHTSensor) Start(readingsChan chan<- Reading) {
 	if s.Emitting == true {
 		// Sensor is running already, stop it before we continue
 		s.Stop()
 	}
 
-	s.pin = pin
 	s.readingsChan = readingsChan
-	s.ticker = time.NewTicker(time.Second * time.Duration(delay))
+	fmt.Printf("%d", s.Delay)
+	s.ticker = time.NewTicker(time.Second * time.Duration(s.Delay))
 	s.Emitting = true
 
 	// Read and emit
